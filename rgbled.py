f51fd1e5f283f3fc53a4037c9d0c29a72410ee0a
Kamil Worek <kamil.worek@protonmail.com>
2020-11-26 14:42:54 +0100

    added CIE 1931 LUT generator

--- rgbled.py
+++ rgbled.py
@@ -1,52 +1,14 @@
 import math
 import time
+import pwm_lightness
 from machine import Pin, PWM
 from neopixel import NeoPixel
 
 # -----------------------------------------------------------------------------
-# gamma LUTs
+# CIE 1931 LUTs
 
-# Gamma brightness lookup table <https://victornpb.github.io/gamma-table-generator>
-# gamma = 2.00 steps = 256 range = 0-1023
-gamma_lut_10b = (
-     0,   0,   0,   0,   0,   0,   1,   1,   1,   1,   2,   2,   2,   3,   3,   4,
-     4,   5,   5,   6,   6,   7,   8,   8,   9,  10,  11,  11,  12,  13,  14,  15,
-    16,  17,  18,  19,  20,  22,  23,  24,  25,  26,  28,  29,  30,  32,  33,  35,
-    36,  38,  39,  41,  43,  44,  46,  48,  49,  51,  53,  55,  57,  59,  60,  62,
-    64,  66,  69,  71,  73,  75,  77,  79,  82,  84,  86,  88,  91,  93,  96,  98,
-   101, 103, 106, 108, 111, 114, 116, 119, 122, 125, 127, 130, 133, 136, 139, 142,
-   145, 148, 151, 154, 157, 160, 164, 167, 170, 173, 177, 180, 184, 187, 190, 194,
-   197, 201, 204, 208, 212, 215, 219, 223, 227, 230, 234, 238, 242, 246, 250, 254,
-   258, 262, 266, 270, 274, 278, 282, 287, 291, 295, 300, 304, 308, 313, 317, 322,
-   326, 331, 335, 340, 345, 349, 354, 359, 363, 368, 373, 378, 383, 388, 393, 398,
-   403, 408, 413, 418, 423, 428, 434, 439, 444, 449, 455, 460, 465, 471, 476, 482,
-   487, 493, 498, 504, 510, 515, 521, 527, 533, 538, 544, 550, 556, 562, 568, 574,
-   580, 586, 592, 598, 604, 611, 617, 623, 629, 636, 642, 648, 655, 661, 668, 674,
-   681, 687, 694, 700, 707, 714, 720, 727, 734, 741, 748, 755, 761, 768, 775, 782,
-   789, 796, 804, 811, 818, 825, 832, 839, 847, 854, 861, 869, 876, 884, 891, 899,
-   906, 914, 921, 929, 937, 944, 952, 960, 968, 975, 983, 991, 999,1007,1015,1023
-)
-
-# Gamma brightness lookup table <https://victornpb.github.io/gamma-table-generator>
-# gamma = 2.00 steps = 256 range = 0-255
-gamma_lut_8b = (
-     0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   1,   1,   1,   1,
-     1,   1,   1,   1,   2,   2,   2,   2,   2,   2,   3,   3,   3,   3,   4,   4,
-     4,   4,   5,   5,   5,   5,   6,   6,   6,   7,   7,   7,   8,   8,   8,   9,
-     9,   9,  10,  10,  11,  11,  11,  12,  12,  13,  13,  14,  14,  15,  15,  16,
-    16,  17,  17,  18,  18,  19,  19,  20,  20,  21,  21,  22,  23,  23,  24,  24,
-    25,  26,  26,  27,  28,  28,  29,  30,  30,  31,  32,  32,  33,  34,  35,  35,
-    36,  37,  38,  38,  39,  40,  41,  42,  42,  43,  44,  45,  46,  47,  47,  48,
-    49,  50,  51,  52,  53,  54,  55,  56,  56,  57,  58,  59,  60,  61,  62,  63,
-    64,  65,  66,  67,  68,  69,  70,  71,  73,  74,  75,  76,  77,  78,  79,  80,
-    81,  82,  84,  85,  86,  87,  88,  89,  91,  92,  93,  94,  95,  97,  98,  99,
-   100, 102, 103, 104, 105, 107, 108, 109, 111, 112, 113, 115, 116, 117, 119, 120,
-   121, 123, 124, 126, 127, 128, 130, 131, 133, 134, 136, 137, 139, 140, 142, 143,
-   145, 146, 148, 149, 151, 152, 154, 155, 157, 158, 160, 162, 163, 165, 166, 168,
-   170, 171, 173, 175, 176, 178, 180, 181, 183, 185, 186, 188, 190, 192, 193, 195,
-   197, 199, 200, 202, 204, 206, 207, 209, 211, 213, 215, 217, 218, 220, 222, 224,
-   226, 228, 230, 232, 233, 235, 237, 239, 241, 243, 245, 247, 249, 251, 253, 255,
-  )
+cie_lut_10b = pwm_lightness.get_pwm_table(1023, 255)
+cie_lut_8b = pwm_lightness.get_pwm_table(255, 255)
 
 # -----------------------------------------------------------------------------
 # RGB PWM LED
@@ -65,10 +27,10 @@ def init(rpin, gpin, bpin):
 
 def set(r, g, b):
 
-    gamma = gamma_lut_10b
-    _rpwm.duty(gamma[r])
-    _gpwm.duty(gamma[g])
-    _bpwm.duty(gamma[b])
+    cie = cie_lut_10b
+    _rpwm.duty(cie[r])
+    _gpwm.duty(cie[g])
+    _bpwm.duty(cie[b])
 
 # -----------------------------------------------------------------------------
 # RGB LED strip
@@ -84,11 +46,11 @@ def strip_init(pin, leds):
 def strip_set(r, g, b):
 
     drv = _led_strip_drv
-    gamma = gamma_lut_8b
+    cie = cie_lut_8b
     leds = range(drv.n)
 
     for led in leds:
-        drv[led] = (gamma[r], gamma[g], gamma[b])
+        drv[led] = (cie[r], cie[g], cie[b])
     drv.write()
 
 def _hsv2rgb(h, s, v):
@@ -104,37 +66,37 @@ def _hsv2rgb(h, s, v):
 def strip_rainbow():
 
     drv = _led_strip_drv
-    gamma = gamma_lut_8b
+    cie = cie_lut_8b
     hue_step = 360 / drv.n
     leds = range(drv.n)
     
     for led in leds:
         avg_hue = hue_step * (2 * led + 1) / 2
         (r, g, b) = _hsv2rgb(avg_hue, 1.0, 1.0)
-        drv[led] = (gamma[r], gamma[g], gamma[b])
+        drv[led] = (cie[r], cie[g], cie[b])
     drv.write()
 
 def strip_breathe():
 
     drv = _led_strip_drv
-    gamma = gamma_lut_8b
-    steps = range(0, 128)
+    cie = cie_lut_8b
+    steps = range(0, 256)
     leds = range(0, drv.n * 3, 3)
     color = bytearray(3)
 
     for step in steps:
         for led in leds:
-            color[1] = gamma[step]
-            color[0] = gamma[step]
-            color[2] = gamma[step]
+            color[1] = cie[step]
+            color[0] = cie[step]
+            color[2] = cie[step]
             drv.buf[led : led + 3] = color
         drv.write()
 
     steps = reversed(steps)
     for step in steps:
         for led in leds:
-            color[1] = gamma[step]
-            color[0] = gamma[step]
-            color[2] = gamma[step]
+            color[1] = cie[step]
+            color[0] = cie[step]
+            color[2] = cie[step]
             drv.buf[led : led + 3] = color
         drv.write()
