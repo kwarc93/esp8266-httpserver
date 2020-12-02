def6ffea9026a4a25aadd07c255e481e0cba4460
Kamil Worek <kamil.worek@protonmail.com>
2020-12-02 12:10:46 +0100

    added rainbow mode

--- main.py
+++ main.py
@@ -36,21 +36,33 @@ def handle_main_page(conn, body):
 
 def handle_rgb(conn, body):
 
+    headers = {
+        'Connection': 'close',
+        'ETag': '\"' + str(body) + '\"'
+    }
+    conn.write(httpserver.create_header(headers, 204))
+
     regex = re.compile('(rgb)\((\d+), (\d+), (\d+)\)')
     regex = regex.match(body)
 
     r = int(regex.group(2))
     g = int(regex.group(3))
     b = int(regex.group(4))
-    # rgbled.set(r,g,b)
-    # rgbled.strip_set(r, g, b)
     rgbled.strip_set_smooth(r, g, b)
 
+def handle_rainbow(conn, body):
+
+    html_file = 'rainbow.html'
+
     headers = {
-        'Connection': 'close',
-        'ETag': '\"' + str(body) + '\"'
+        'Content-Type': 'text/html',
+        'Content-Length': str(os.stat(html_file)[6]),
+        'Connection': 'close'
     }
-    conn.write(httpserver.create_header(headers, 204))
+    conn.write(httpserver.create_header(headers, 200))
+    conn.write(read_file(html_file))
+
+    rgbled.strip_rainbow()
 
 def handle_favicon(conn, body):
 
@@ -75,12 +87,12 @@ def handle_not_found(conn, body):
 print("--- main.py ---")
 
 rgbled.strip_init(5, 60)
-rgbled.strip_rainbow()
 
 httpserver.init(credentials.ssid, credentials.pwd, True)
 
 httpserver.register_callback('GET', '/', handle_main_page)
 httpserver.register_callback('GET', '/favicon.ico', handle_favicon)
+httpserver.register_callback('GET', '/rainbow', handle_rainbow)
 httpserver.register_callback('POST', '/rgb', handle_rgb)
 httpserver.register_not_found_callback(handle_not_found)
 
